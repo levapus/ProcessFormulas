b7c671141d7fbc898c2e58eb7be2b3daa6d85a0a
BalanP <nalab.levap@gmail.com>
2020-04-14 18:41:55 +0200

    Select subgraph with a new root node. This is the solution to the first of two major problems: 1. build a subgraph and 2. update formula with dependencies on parent formulas

--- src/main/scala/ProcessFormulas.scala
+++ src/main/scala/ProcessFormulas.scala
@@ -1,8 +1,11 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, Row, SparkSession}
 import org.apache.spark.sql.functions._
+
 import org.graphframes._
 import org.graphframes.lib.{AggregateMessages, Pregel}
+import org.apache.spark.graphx.{EdgeDirection, EdgeTriplet, VertexId}
+
 
 object ProcessFormulas extends App {
   val sparkSession = SparkSession.builder()
@@ -16,7 +19,7 @@ object ProcessFormulas extends App {
 
   //building the graph
   // Create a Vertex DataFrame with unique ID column "id"
-  val v = sqlContext.createDataFrame(List(
+  val verticesDF = sqlContext.createDataFrame(List(
     ("r00t", "root", "", 0),
     ("b", "formula B", "", 1),
     ("c", "formula C", "", 2),
@@ -36,9 +39,15 @@ object ProcessFormulas extends App {
     ("q", "formula Q", "", 16),
     ("r", "formula R", "", 17)
   )).toDF("id", "name", "formula", "value")
+    //processing columns
+    .withColumn("level", lit(null))
+    .withColumn("root", lit(null))
+    .withColumn("path", lit(null))
+    .withColumn("iscyclic", lit(null))
+    .withColumn("isleaf", lit(null))
 
   // Create an Edge DataFrame with "src" and "dst" columns
-  val e = sqlContext.createDataFrame(List(
+  val edgesDF = sqlContext.createDataFrame(List(
     ("r00t", "b"),
     ("r00t", "c"),
     ("r00t", "d"),
@@ -49,10 +58,8 @@ object ProcessFormulas extends App {
     ("c", "i"),
     ("c", "l"),
     ("d", "i"),
-    ("d", "i"),
     ("d", "m"),
     ("d", "n"),
-    ("d", "o"),
     ("e", "q"),
     ("f", "h"),
     ("f", "j"),
@@ -68,46 +75,112 @@ object ProcessFormulas extends App {
   )).toDF("src", "dst")
 
   // Create a GraphFrame
-  val g = GraphFrame(v, e)
+  val g = GraphFrame(verticesDF, edgesDF)
 
   //Display the vertex and edge DataFrames
-  g.vertices.show()
-  g.edges.show()
+  println("=== Vertices: ===========================================================")
+  g.vertices.show(100)
 
+  println("=== Edges: ==============================================================")
+  g.edges.show(100)
 
-  // Search for pairs of vertices with edges in both directions between them.
-  val motifs = g.find("(a)-[e]->(b)")
-  motifs.show()
+  //  Select subgraph for a given root
+//  val newRoot = "d"
+  val newRoot = "r00t"
+  println(s"=== Select subgraph with root ($newRoot) ===============================")
 
-  println("BFS ------------------------------------------")
-  // Search from "Esther" for users of age < 32.
-  val paths = g.bfs.fromExpr("id = 'r00t'").toExpr("id = 'r'").run()
-  paths.show()
+  /**
+   * Calculate subgraph based on new root vertex
+   *
+   * @param graph
+   * @param newRoot
+   * @return
+   */
+  def subgraph(graph: GraphFrame, newRoot: String) = {
+    val vertices = g.vertices.select("id").collect.map(r => r.getString(0)).toList
+    val shortestPathsDF = graph.shortestPaths.landmarks(vertices).run()
+      .select("id", "distances")
+        .where(s"id = '$newRoot'")
 
-  println("Message passing via AggregateMessages -------------------------------------------------------")
+    //debug info
+    shortestPathsDF.show(false)
 
-  // We will use AggregateMessages utilities later, so name it "AM" for short.
-  val AM = AggregateMessages
+    val newVertices = shortestPathsDF.first().getMap(1).keySet
 
-  // For each user, sum the ages of the adjacent users.
-//  val msgToSrc = AM.dst("age")
-  val msgToDst = AM.src("value")
-  val agg = { g.aggregateMessages
-//    .sendToSrc(msgToSrc)  // send destination user's age to source
-    .sendToDst(msgToDst)  // send source user's age to destination
-    .agg(sum(AM.msg).as("summedValues")) } // sum up ages, stored in AM.msg column
-  agg.show()
+    g.filterVertices(s"id in (${newVertices.mkString("'", "', '", "'")})").dropIsolatedVertices()
+  }
 
+  val subgr = subgraph(g, newRoot)
+  println("Subgraph Vertices: ")
+  subgr.vertices.show(100)
 
-  val numVertices = g.vertices.count()
-  val alpha = 0.15
-  val ranks = g.pregel
-    .withVertexColumn("rank", lit(1.0 / numVertices),
-      coalesce(Pregel.msg, lit(0.0)) * (1.0 - alpha) + alpha / numVertices)
-    .sendMsgToDst(Pregel.src("rank") / Pregel.src("value"))
-    .aggMsgs(sum(Pregel.msg))
-    .run()
+  println("Subgraph Edges: ")
+  subgr.edges.show(100)
 
-  ranks.show()
+  val gx = g.toGraphX
+
+//  val numVertices = g.vertices.count()
+//  val alpha = 0.15
+//  val ranks = g.pregel
+//    .withVertexColumn("rank", lit(1.0 / numVertices),
+//      coalesce(Pregel.msg, lit(0.0)) * (1.0 - alpha) + alpha / numVertices)
+//    .sendMsgToDst(Pregel.src("rank") / Pregel.src("value"))
+//    .aggMsgs(sum(Pregel.msg))
+//    .run()
+//
+//  ranks.show()
+
+//  val processedRDD = gx.pregel(initialMsg,
+//    Int.MaxValue,
+//    EdgeDirection.Out)(
+//    setMsg,
+//    sendMsg,
+//    mergeMsg)
+//
+//
+//  // initialize id,level,root,path,iscyclic, isleaf
+//  val initialMsg = (0L,0,0.asInstanceOf[Any],List("dummy"),0,1)
+//
+//  //mutate the value of the vertices
+////  def setMsg(vertexId: VertexId, value: (Long,Int,Any,List[String], Int,String,Int,Any), message: (Long,Int, Any,List[String],Int,Int)): (Long,Int, Any,List[String],Int,String,Int,Any) = {
+//  def setMsg(vertexId: VertexId, value: Row, message: (Long,Int, Any,List[String],Int,Int)): Row = {
+//    if (message._2 < 1) { //superstep 0 - initialize
+//      Row(value.get(0), value.get(1),value.get(2),value.get(3),value.get(4),value.get(5),value.get(6),value.get(7))
+//    } else if ( message._5 == 1) { // set isCyclic
+//      (value._1, value._2, value._3, value._4, message._5, value._6, value._7,value._8)
+//    } else if ( message._6 == 0 ) { // set isleaf
+//      (value._1, value._2, value._3, value._4, value._5, value._6, message._6,value._8)
+//    } else { // set new values
+//      ( message._1,value._2+1, message._3, value._6 :: message._4 , value._5,value._6,value._7,value._8)
+//    }
+//  }
+//
+//  // send the value to vertices
+//  def sendMsg(triplet: EdgeTriplet[(Long,Int,Any,List[String],Int,String,Int,Any), _]): Iterator[(VertexId, (Long,Int,Any,List[String],Int,Int))] = {
+//    val sourceVertex = triplet.srcAttr
+//    val destinationVertex = triplet.dstAttr
+//    // check for icyclic
+//    if (sourceVertex._1 == triplet.dstId || sourceVertex._1 == destinationVertex._1)
+//      if (destinationVertex._5==0) { //set iscyclic
+//        Iterator((triplet.dstId, (sourceVertex._1, sourceVertex._2, sourceVertex._3,sourceVertex._4, 1,sourceVertex._7)))
+//      } else {
+//        Iterator.empty
+//      }
+//    else {
+//      if (sourceVertex._7==1) //is NOT leaf
+//        {
+//          Iterator((triplet.srcId, (sourceVertex._1,sourceVertex._2,sourceVertex._3, sourceVertex._4 ,0, 0 )))
+//        }
+//        else { // set new values
+//        Iterator((triplet.dstId, (sourceVertex._1, sourceVertex._2, sourceVertex._3, sourceVertex._4, 0, 1)))
+//      }
+//    }
+//  }
+//
+//  // receive the values from all connected vertices
+//  def mergeMsg(msg1: (Long,Int,Any,List[String],Int,Int), msg2: (Long,Int, Any,List[String],Int,Int)): (Long,Int,Any,List[String],Int,Int) = {
+//    // dummy logic not applicable to the data in this use case
+//    msg2
+//  }
 
 }
