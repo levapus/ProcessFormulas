ec4226986c8753d23e499946edd32d1bcde2336e
BalanP <nalab.levap@gmail.com>
2020-04-18 17:25:50 +0200

    Bingo! Formula processing started to work! Now we can compute complex formulas which references to parent formulas.

--- src/main/scala/ProcessFormulas.scala
+++ src/main/scala/ProcessFormulas.scala
@@ -22,9 +22,9 @@ object ProcessFormulas extends App {
     ("r00t", "root", "", 0),
     ("b", "formula B", "1", 0),
     ("c", "formula C", "item(q) + 1", 0),
-    ("d", "formula D", "3", 0),
+    ("d", "formula D", "1", 0),
     ("e", "formula E", "item(b) + 1", 0),
-    ("f", "formula F", "item(b) + 1", 0),
+    ("f", "formula F", "item(b) + item(c)", 0),
     ("g", "formula G", "item(b) + 1", 0),
     ("h", "formula H", "item(g) + item(f)", 0),
     ("i", "formula I", "item(c) + item(d)", 0),
@@ -48,7 +48,6 @@ object ProcessFormulas extends App {
   //create an Edge DataFrame with "src" and "dst" columns
   val edgesDF = sqlContext.createDataFrame(List(
     ("r00t", "b"),
-//    ("r00t", "c"),
     ("r00t", "d"),
     ("b", "e"),
     ("b", "f"),
@@ -100,9 +99,9 @@ object ProcessFormulas extends App {
   // edge Edge(src_vId, dst_vId, edgeAttr) = Edge(srcVertexId, dstVertexId, (formula src, formula dst))
   val preprocessedGx = gx.mapVertices((id, row) =>
     if(row.getString(0) == "r00t")
-      (row.getString(0), row.getString(2), 0D, 0D)
+      (row.getString(0), row.getString(2), 0D, 0D, Map[String, Double]())
     else
-      (row.getString(0), row.getString(2), Double.NegativeInfinity, Double.PositiveInfinity))
+      (row.getString(0), row.getString(2), Double.NegativeInfinity, Double.PositiveInfinity, Map[String, Double]()))
       .mapEdges(ed => (ed.attr.getString(0), ed.attr.getString(1)))
 
   preprocessedGx.vertices.foreach(x => println(x))
@@ -115,41 +114,47 @@ object ProcessFormulas extends App {
   val processedGx = preprocessedGx.pregel(
     //initial message
     initialMsg,
-//    ("", Double.PositiveInfinity, Map[String, Double]()),
-    Int.MaxValue, EdgeDirection.Out)(
+    Int.MaxValue,
+    EdgeDirection.Out)(
     //vprog - vertex Program - mutate the value of the vertices
     (id, vAttr, msg) => {
       println(s"vprog: $vAttr with msg: $msg")
       if(msg._1 == Double.NegativeInfinity) {
         //superstep 0 - initialize
-        val init_vAttr = (vAttr._1, vAttr._2, math.max(vAttr._3, msg._1), vAttr._4)
+        val init_vAttr = (vAttr._1, vAttr._2, math.max(vAttr._3, msg._1), vAttr._4, vAttr._5)
         println(s"  superstep 0 - initialize: $init_vAttr")
         init_vAttr
       } else {
         //set new values
         var formula = vAttr._2
-//        println(s"${vAttr._1} formula: " + formula + ", args: " + msg._2)
-//        msg._2.keySet.foreach(parentId => {
-//          formula = formula.replaceAll(s"item\\($parentId\\)", msg._2(parentId).toInt.toString)
-//        })
-//        println(s"${vAttr._1} formula: $formula")
-//        val a: Option[() => Int] = ArithmeticParser.readExpression(formula)
-//        (vAttr._1, formula, math.max(vAttr._3, msg._1), a.get())
-        val new_vAttr = (vAttr._1, formula, math.max(vAttr._3, msg._1), vAttr._4)
+        println(s"${vAttr._1} formula: " + formula + ", args: " + msg._2)
+        msg._2.keySet.foreach(parentId => {
+          formula = formula.replaceAll(s"item\\($parentId\\)", msg._2(parentId).toInt.toString)
+        })
+        println(s"${vAttr._1} formula: $formula")
+        val newFormulaValue = if (!formula.contains("item"))
+          ArithmeticParser.readExpression(formula).get() else vAttr._4
+
+        val new_vAttr = (vAttr._1, formula, math.max(vAttr._3, msg._1), newFormulaValue, vAttr._5 ++ msg._2)
+
         println(s"  set new values: $new_vAttr")
         new_vAttr
       }
-//      (vAttr._1, vAttr._2, math.max(vAttr._3, msg._1), vAttr._4)
     },
+
     //sendMsg - send Message - send the value to vertices
     triplet => {
-      if (triplet.srcAttr._3 != Double.PositiveInfinity) {
-        println(s"sendMsg from ${triplet.srcAttr._1} to ${triplet.dstAttr._3}: " + (triplet.dstId, (triplet.srcAttr._3 + 1, Map(triplet.srcAttr._1 -> triplet.srcAttr._4))))
-        Iterator((triplet.dstId, (triplet.srcAttr._3 + 1, Map(triplet.srcAttr._1 -> triplet.srcAttr._4))))
+      if (triplet.srcAttr._3 != Double.NegativeInfinity) {
+        val parentValues = if(triplet.srcAttr._4 != Double.PositiveInfinity)
+          Map(triplet.srcAttr._1 -> triplet.srcAttr._4) else Map[String, Double]()
+
+        println(s"sendMsg from ${triplet.srcAttr._1} to ${triplet.dstAttr._1}: " + (triplet.dstId, (triplet.srcAttr._3 + 1, parentValues)))
+        Iterator((triplet.dstId, (triplet.srcAttr._3 + 1, parentValues)))
       } else {
         Iterator.empty
       }
     },
+
     //mergeMsg - merge message - receive the values from all connected vertices
     (msg1, msg2) => {
       println(s"mergeMsg: ${msg1._1}, ${msg2._1}")
